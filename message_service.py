eb3e91d756dff1d18a4c2f5fe8da20c00f4f94cb
oleg_androsiuk <a.oleg1408@gmail.com>
2025-10-09 00:37:02 +0200

    Fixing message_service, solving  problem with big messages

--- message_service.py
+++ message_service.py
@@ -44,7 +44,8 @@ class SteganographyManager(Service):
         return width * height
 
     def get_binary_string(self, msg):
-        bin_list = [bin(ord(x))[2:].zfill(8) for x in msg]
+        bytes_msg = msg.encode('utf-8')
+        bin_list = [bin(b)[2:].zfill(8) for b in bytes_msg]
         binary_msg = ''.join(bin_list)
         return binary_msg
 
@@ -66,62 +67,45 @@ class SteganographyManager(Service):
             end += 3
 
     def is_even(self, num):
-        if num % 2 == 0:
-            return True
-        else:
-            return False
+        return num % 2 == 0
 
     def mod_bitmap(self, bitmap, bstring):
         bitmap = [list(p) for p in bitmap]
-
         bstring_start = 0
         bstring_end = 8
         substring_start = 0
         substring_end = 3
         end_pixel_list = 0
 
-        for x in range(0, len(bitmap)):
+        for x in range(len(bitmap)):
             p = 0
-
             if x % 3 == 0:
                 substring = bstring[bstring_start:bstring_end]
                 bstring_start += 8
                 bstring_end += 8
                 substring_start = 0
-                substring_end = 3
+                substring_end = 3 if x < len(bitmap) - 3 else 2
 
             for y in substring[substring_start:substring_end]:
                 if y == "0":
                     if self.is_even(bitmap[x][p]):
                         pass
                     else:
-                        if bitmap[x][p] == 0:
-                            bitmap[x][p] += 1
-                        else:
-                            bitmap[x][p] -= 1
+                        bitmap[x][p] = 1 if bitmap[x][p] == 0 else bitmap[x][p] - 1
                 else:
-                    if self.is_even(bitmap[x][p]):
-                        if bitmap[x][p] == 0:
-                            bitmap[x][p] += 1
-                        else:
-                            bitmap[x][p] -= 1
-                    else:
+                    if not self.is_even(bitmap[x][p]):
                         pass
+                    else:
+                        bitmap[x][p] = 1 if bitmap[x][p] == 0 else bitmap[x][p] - 1
                 p += 1
 
             if end_pixel_list == 2:
                 if x == len(bitmap) - 1:
                     if self.is_even(bitmap[x][2]):
-                        if bitmap[x][2] == 0:
-                            bitmap[x][2] += 1
-                        else:
-                            bitmap[x][2] -= 1
+                        bitmap[x][2] = 1 if bitmap[x][2] == 0 else bitmap[x][2] - 1
                 else:
                     if not self.is_even(bitmap[x][2]):
-                        if bitmap[x][2] == 0:
-                            bitmap[x][2] += 1
-                        else:
-                            bitmap[x][2] -= 1
+                        bitmap[x][2] = 1 if bitmap[x][2] == 0 else bitmap[x][2] - 1
                 end_pixel_list = 0
             else:
                 end_pixel_list += 1
@@ -133,7 +117,7 @@ class SteganographyManager(Service):
 
     def inject_bitmap(self, bitmap, img):
         width = img.size[0]
-        (x, y) = (0, 0)
+        x, y = 0, 0
         for pixel in bitmap:
             img.putpixel((x, y), tuple(pixel))
             if x == width - 1:
@@ -149,16 +133,20 @@ class SteganographyManager(Service):
         if not self.text_fits(message, image):
             if os.path.exists(path):
                 os.remove(path)
-            raise ValueError("Message is too big.")
+            raise ValueError("Message is too big for the image.")
         bstring = self.get_binary_string(message)
         pixel_count = len(message) * 3
         pixels = self.get_pixels(pixel_count, image)
         bitmaps = list(self.get_bitmaps(pixels))
+        if len(bstring) > len(bitmaps) * 8:
+            if os.path.exists(path):
+                os.remove(path)
+            raise ValueError("Binary string too long for bitmap.")
         new_bitmap = self.mod_bitmap(bitmaps, bstring)
         new_img = self.inject_bitmap(new_bitmap, image)
         base, _ = os.path.splitext(path)
         modified_path = f"{base}_stego.png"
-        new_img.save(modified_path)
+        new_img.save(modified_path, format='PNG')
         encrypted_message = "#############"
         self.repo.save(text=encrypted_message, photo_path=modified_path)
         if os.path.exists(path):
@@ -166,12 +154,14 @@ class SteganographyManager(Service):
 
     def decode(self, img):
         pixels = self.get_pixels(self.get_pixel_count(img), img)
-        if self.is_even(pixels[2][2]):
-            i = 2
-            while self.is_even(pixels[i][2]):
+        i = 2
+        try:
+            while i < len(pixels) and self.is_even(pixels[i][2]):
                 i += 3
-        else:
-            i = 2
+            if i >= len(pixels):
+                raise ValueError("No valid termination flag found.")
+        except IndexError:
+            raise ValueError("Invalid image or corrupted data.")
         bstring = self.translate_pixels(pixels[0:i + 1])
         string = self.translate_from_binary(bstring)
         return string
@@ -180,19 +170,24 @@ class SteganographyManager(Service):
         bstring = ""
         for byte in pixels:
             for b in byte:
-                if int(b) % 2 == 0:
-                    bstring += "0"
-                else:
-                    bstring += "1"
+                bstring += "0" if self.is_even(b) else "1"
         return bstring
 
     def translate_from_binary(self, bstring):
         string = ""
-        for b in range(0, len(bstring), 9):
-            st = chr(int(bstring[b:b + 8], 2))
-            string += st
+        for b in range(0, len(bstring) - 8, 9):
+            try:
+                char_bits = bstring[b:b + 8]
+                if len(char_bits) == 8:
+                    char = chr(int(char_bits, 2))
+                    string += char
+            except ValueError:
+                break
         return string
 
     def decrypt_message(self, photo_path):
-        image = Image.open(photo_path).convert('RGB')
-        return self.decode(image)
+        try:
+            image = Image.open(photo_path).convert('RGB')
+            return self.decode(image)
+        except Exception as e:
+            raise ValueError(f"Failed to decrypt image: {str(e)}")
